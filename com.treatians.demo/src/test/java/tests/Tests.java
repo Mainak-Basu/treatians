c4e9faf99b5ed0ec9db7bb029467212c79e0ff5a
mainak basu <mainakbasu4@gmail.com>
2023-02-08 14:26:13 +0530

    chal ja bhai

--- com.treatians.demo/src/test/java/tests/Tests.java
+++ com.treatians.demo/src/test/java/tests/Tests.java
@@ -8,12 +8,15 @@ import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import generic_utilities.Base_Class;
+import generic_utilities.Data_Providers;
 import generic_utilities.treatians_listener;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import treatians_pages.Book_Appointments;
+import treatians_pages.Create_new_user_page;
+import treatians_pages.Login;
 @Listeners(treatians_listener.class)
-public class Book_Appointment_test extends Base_Class{
+public class Tests extends Base_Class{
 	@Test(priority=1)
 	public void book_ap() throws IOException {
 		Base_Class bc=new Base_Class();
@@ -39,7 +42,46 @@ public class Book_Appointment_test extends Base_Class{
 		driver.pressKey(new KeyEvent(AndroidKey.BACK));
 		ba.menu();
 		ba.logout.click();
-		
+		}
+	@Test(enabled=false)
+	public void createnewuser() {
+		Create_new_user_page c= new Create_new_user_page(driver);
+		driver.activateApp("com.treatians");
+		c.clickOnMenu();
+		c.clickOnLogin();
+		c.createNewUser();
+		c.enterFirstName();
+		c.enterLastName();
+		c.enterEmail();
+		c.back();
+		c.phone();
+		c.enternumber();
+		c.enterpassword();
+		c.confirmpassword();
+		c.back();
+		c.clickcountrydropdown();
+		c.selectCountry();
+		c.clickcitydropdown();
+		c.selectcity();
+		c.enterAddress();
+		c.back();
+		c.scroll();
+		c.clickRegister();
 		
 	}
+	
+		@Test(priority=4, dataProvider = "excelTest", dataProviderClass = Data_Providers.class)
+		public void logintest(String email, String password){
+			Login l=new Login(driver);
+			driver.activateApp("com.treatians");
+			l.menu();
+			l.login();
+			l.Email_text_field.sendKeys(email);
+			l.password_text_field.sendKeys(password);
+			l.signin();
+			Assert.assertTrue(l.successfully_loggedin.isDisplayed());
+			l.menu();
+			l.logout();
+		}
+		
 	}
